e3b44c660afaf3d9742e997a4dfeaa95d2404f11
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-09-08 13:39:06 +0300

    внесла путь к  базе данных

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -2,12 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_PUBLIC_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    use_env_variable: 'DATABASE_PUBLIC_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
 
